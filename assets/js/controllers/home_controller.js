78351cce4cb15e804b67dc75618ef08711cacf55
David <dgrayboff@gmail.com>
2016-09-14 17:13:31 -0400

    added sign in modal

--- assets/js/controllers/home_controller.js
+++ assets/js/controllers/home_controller.js
@@ -2,8 +2,26 @@ cycleApp.controller('homeCtrl', function($scope){
 
   console.log("homeCtrl linked");
 
+  $scope.signUpModal = true;
   $scope.signUp = function(){
     console.log('signup click');
+    $scope.signUpModal = false;
   };
+
+  $scope.closeSignUp = function(){
+    console.log('close signup');
+    $scope.signUpModal = true;
+  }
+
+  $scope.logInModal = true;
+  $scope.logIn = function(){
+    console.log('login click');
+    $scope.logInModal = false;
+  }
+
+  $scope.closeLogIn = function(){
+    console.log('close login');
+    $scope.logInModal = true;
+  }
 
 });
